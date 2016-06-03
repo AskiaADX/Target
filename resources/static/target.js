65b2639fbc12086d48144b1e5ffb882f3b8e4ef9
jackalds <jackaldesigns@gmail.com>
2016-06-03 17:47:11 +0100

    Continuing build

--- resources/static/target.js
+++ resources/static/target.js
@@ -101,17 +101,22 @@
 			images_loaded = 0,
 			enableMobileFit = false,
             mouseMove = false,
+            smartView = false,
             useAltCircle = Boolean(options.useAltCircle);
 		
         var isMobile = false; //initiate as false
 		// device detection
 		if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
-        
-        if ( isMobile ) {
+
+        if ( isMobile || parseInt(document.documentElement.clientWidth) < 400 ) {
             // if mobile set widths to 100%
-            $container.find('.startArea').width('100%');
-            $container.find('.targetContainer').width('100%');
+            smartView = true;
+            stackResponses = true;
+            $container.find('.startArea').width('100%').css({'float':'none','height':''});
+            $container.find('.targetContainer').width('100%').height($container.find('.startArea').outerHeight()).css({'left':'0'/*,'position':'relative'*/});
+        } else {
+            
         }
         
         
@@ -300,13 +305,17 @@
 		if ( stackResponses ) {
 			
 			for ( var i=($('.responseItem').size()-1); i>=0; i-- ) {
-		
-				var offset = $('.responseItem').eq(0).offset();
+				$('.responseItem').eq(i).css({
+					'left' : (($container.find('.startArea').outerWidth()*0.5) - ($('.responseItem').eq(i).outerWidth(true)*0.5)) + 'px',
+					'top' : 0
+				});
 				$('.responseItem').eq(i).css("position", "absolute");
+				var offset = $('.responseItem').eq(i).offset();
+				
 				$('.responseItem').eq(i).offset(offset);
                 $('.responseItem').eq(i).data({
                     'oTop':$('.responseItem').eq(i).offset().top,
-                    'oLeft':$('.responseItem').eq(i).offset().left,
+                    'oLeft':(($container.find('.startArea').outerWidth()*0.5) - ($('.responseItem').eq(i).outerWidth(true)*0.5)) + 'px',
                     'oHeight':$('.responseItem').eq(i).outerHeight(),
                     'oWidth':$('.responseItem').eq(i).outerWidth()
                 });
@@ -323,9 +332,19 @@
 		}
         
         /* Circles */
+        $container.find('.circle0').width('100%');
+        if ( smartView ) {
+            var bodyPadding = $('body').outerWidth(true) - $('body').outerWidth(false);
+            if( ($(window).outerHeight(false) - $container.find('.startArea').outerHeight(true) - bodyPadding) > $container.find('.targetContainer').outerWidth())
+            	$container.find('.circle0').width( $container.find('.targetContainer').outerWidth() );
+            else
+                $container.find('.circle0').width( $(window).outerHeight(false) - $container.find('.startArea').outerHeight(true) - bodyPadding);
+            $container.find('.circle0').css('left',(($container.find('.targetContainer').outerWidth()/2) - $container.find('.circle0').outerWidth()/2) +'px')
+        }
+        var outerCircleWidth = $container.find('.circle0').width();
         var totalNumberOfCircles = options.scaleMax - options.scaleMin + 1;
-		var circleSizeDiff = (options.outerCircleWidth - options.innerCircleWidth) / totalNumberOfCircles;
-		var currentCircleWidth = options.outerCircleWidth;
+		var circleSizeDiff = (outerCircleWidth - options.innerCircleWidth) / totalNumberOfCircles;
+		var currentCircleWidth = outerCircleWidth;
 		var previousCircle = $container.find('.targetContainer');
         var alt = 0;
         		
@@ -345,6 +364,9 @@
                     'border-radius':		(currentCircleWidth/2) + 'px' 
                 })
                 .data('index',i);
+           	if ( smartView ) {
+                $container.find('.circle0').css('left',(($container.find('.targetContainer').outerWidth()/2) - $container.find('.circle0').outerWidth()/2) +'px')
+            }
 
             if ( useAltCircle && alt === 1 ) $container.find('.circle' + i).addClass("alt");
             
@@ -360,11 +382,14 @@
         $container.find('.targetContainer').height($container.find('.circle0').height());
         
         // if start area smaller in height than target then increase height
-        if ( $container.find('.startArea').height() < $container.find('.targetContainer').height() ) $container.find('.startArea').height( $container.find('.targetContainer').outerHeight() );
-        
+        if ( $container.find('.startArea').height() < $container.find('.targetContainer').height() && !isMobile && parseInt(document.documentElement.clientWidth) > 400 ) 
+            $container.find('.startArea').height( $container.find('.targetContainer').outerHeight() );
+        else if ( isMobile || parseInt(document.documentElement.clientWidth) < 400 ) {
+           $container.find('.targetContainer').css('top',$container.find('.startArea').outerHeight()+'px') 
+        }
         // if not mobile place side by side
-        if ( !isMobile && options.targetHorizontalPosition == "right" ) $container.find('.targetContainer').css('left','50%');
-        else if ( !isMobile && options.targetHorizontalPosition == "left" ) $container.find('.startArea').css({'left':'50%','position':'absolute'});
+        if ( !isMobile && options.targetHorizontalPosition == "right" && parseInt(document.documentElement.clientWidth) > 400) $container.find('.targetContainer').css('left','50%');
+        else if ( !isMobile && options.targetHorizontalPosition == "left" && parseInt(document.documentElement.clientWidth) > 400) $container.find('.startArea').css({'left':'50%','position':'absolute'});
 		
         // Initialise droppable	
 		$( ".circle" ).droppable({
@@ -399,7 +424,7 @@
 				$( ui.draggable ).transition({ scale: options.scaleOnTarget, 'top':y + 'px', 'left':x + 'px' }).draggable({ cursorAt: { 
 						top:($(ui.draggable).data('oHeight')*options.scaleOnTarget)/2, 
 						left:($(ui.draggable).data('oWidth')*options.scaleOnTarget)/2
-					}, zIndex: 2700 });
+					}, zIndex: 2700 }).attr('data-ontarget',true);
                 										
 				// FIX var currentQuestion = questions[$( ui.draggable ).data('index')];
 				// FIX 	currentQuestion.setValue( parseInt($(this).data('index'))+options.scaleMin );
@@ -411,6 +436,8 @@
 				});	
 				
 				$('html').off("mousemove");
+				$('.responseItem[data-ontarget=false]').hide();
+				$('.responseItem[data-ontarget=false]:hidden:first').show();
 			},
             hoverClass: "over"
 			/*over: function( event, ui ) {
@@ -435,11 +462,14 @@
 				$( ui.draggable ).draggable({revert:'invalid', cursorAt: { 
 					top:($(ui.draggable).data('oHeight'))/2, 
 					left:($(ui.draggable).data('oWidth'))/2 
-				}}).animate({ top:0, left:0 }).transition({ scale: 1 });
+				}}).animate({ top:0, left:$(ui.draggable).data('oLeft') }).transition({ scale: 1 }).attr('data-ontarget',false);
 				
 				// FIX var currentQuestion = questions[$( ui.draggable ).data('index')];
 				// FIX	currentQuestion.clearValues();
                 iterations[$(ui.draggable).data('index')].element.val('');
+				
+				$('.responseItem[data-ontarget=false]').hide();
+				$('.responseItem[data-ontarget=false]:hidden:first').show();
 			},
             hoverClass: "over"
 		});
@@ -492,7 +522,8 @@
 					.transition({ scale: options.scaleOnTarget })
 					.bind('click', function (event) {
 						noDrag(event.target);	
-					});
+					})
+					.attr('data-ontarget',true);
 			} else {
 				// Initialise draggables
 				$('#res'+$(this).data('index')).draggable({ 
@@ -522,8 +553,11 @@
 					}
 				}).bind('click', function (event) {
 					noDrag(event.target);	
-				});
+				})
+				.attr('data-ontarget',false);
 			}
+			
+			$('#res'+$(this).data('index')).hide();
 			
 		});
 			
@@ -552,9 +586,12 @@
 
             $(clickActive).animate({
                 top:e.pageY - $(clickActive).data('top') - ($(clickActive).outerHeight()/2),
-                left:e.pageX - $(clickActive).data('left') - ($(clickActive).outerWidth()/2)
-            }).transition({ scale: options.scaleOnTarget }).data('onTarget',true);
-
+                left:e.pageX - (stackResponses ? 0 : $(clickActive).data('left'))- ($(clickActive).outerWidth()/2)
+            },function() {
+				$('.responseItem[data-ontarget=false]').hide();
+				$('.responseItem[data-ontarget=false]:hidden:first').show();
+			}).transition({ scale: options.scaleOnTarget }).attr('data-ontarget',true);
+			
             /*var currentQuestion = questions[$( clickActive ).data('index')];
                 currentQuestion.setValue( parseInt($(e.target).data('index'))+options.scaleMin );*/
             iterations[$(clickActive).data('index')].element.val( parseInt($(e.target).data('index'))+options.scaleMin );
@@ -581,12 +618,15 @@
 
 						// now all images are loaded.
 						$container.css('visibility','visible');
+						$('.responseItem[data-ontarget=false]:hidden:first').show();
 	
 					}
 				}).attr("src", fakeSrc);
 			});
 		} else {
 			$container.css('visibility','visible');
+			
+			$('.responseItem[data-ontarget=false]:hidden:first').show();
 		}
 		
 		// Returns the container
